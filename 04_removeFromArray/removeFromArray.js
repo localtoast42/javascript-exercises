95956ff50fc67d165ad845285b9494c71ebe97b6
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-01-24 17:05:31 -0500

    Add problem 4 solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...valuesToRemove) {
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (valuesToRemove.includes(arr[i])) {
+            continue;
+        } else {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line

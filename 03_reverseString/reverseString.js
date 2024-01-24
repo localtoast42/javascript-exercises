afa54107308fc1be92f4ba4e2b59a0f9d4187453
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-01-24 16:54:09 -0500

    Add problem 3 solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const arr = string.split("")
+    const arr_reversed = []
+    for (let i = arr.length - 1; i >= 0; i--) {
+        arr_reversed.push(arr[i])
+    }
+    return arr_reversed.join("")
 };
 
 // Do not edit below this line

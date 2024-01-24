9a0ef4feb1046701e6cbeb2cf7d4f0433fd628cd
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-01-24 17:19:27 -0500

    Add problem 5 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if (int1 < 0 || int2 < 0) {
+        return 'ERROR'
+    }
+    if (typeof int1 !== "number" || typeof int2 !== "number") {
+        return 'ERROR'
+    }
+    
+    let start = int1 < int2 ? int1 : int2
+    let end = int1 < int2 ? int2 : int1
+    let answer = 0
+    for (i = start; i <= end; i ++){
+        answer += i
+    }
+    return answer
 };
 
 // Do not edit below this line

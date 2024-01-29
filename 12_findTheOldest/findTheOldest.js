e9a3838cf41eef364d84afd64d4181dd601d8bea
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-01-29 17:32:21 -0500

    Add problem 12 solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let ans = arr.reduce((ans, curr) => getAge(curr) > getAge(ans) ? curr : ans, arr[0]);
+    return ans
+};
 
+const getAge = function(obj) {
+    obj.yearOfDeath = obj.yearOfDeath ? obj.yearOfDeath : new Date().getFullYear();
+    return obj.yearOfDeath - obj.yearOfBirth;
 };
 
 // Do not edit below this line

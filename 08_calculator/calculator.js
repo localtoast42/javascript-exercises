dc8552750c1578e9ed2d558aa947c8b320ebf2b5
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-01-29 17:01:19 -0500

    Add problem 8 solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const answer = arr.reduce((accumulator, curr) => accumulator + curr, 0);
+  return answer;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator, curr) => accumulator * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let answer = 1
+	while (num > 1) {
+    answer *= num;
+    num--;
+  };
+  return answer;
 };
 
 // Do not edit below this line
